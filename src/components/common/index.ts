336546b98ea68e0cea4cd72af6e4cc8f9c5392ca
DRGJ <1276410434@qq.com>
2026-04-04 22:38:08 +0800

    Fix article links for GitHub Pages

--- src/components/common/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Loading } from './Loading';
-export { default as ErrorBoundary } from './ErrorBoundary';
-export { default as ScrollToTop } from './ScrollToTop';
-export { default as BackToTop } from './BackToTop';
