8f92ce235299eea607bfdf55d13ea5062a3f21d1
DRGJ <1276410434@qq.com>
2026-04-09 14:14:35 +0800

    feat: 配置项目支持GitHub Pages部署
    修改vite.config.ts中的base路径为'/article-tutorial-site/'
    添加package.json中的deploy脚本
    将BrowserRouter替换为HashRouter以适应静态页面路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import HomePage from './pages/Home';
 import ArticlesPage from './pages/ArticleList';
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <PerformanceMonitor />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -44,7 +44,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   );
 }
